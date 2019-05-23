7553da2ac8592d99bd2834a2ad27417b365d3e02
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-05-23 15:25:55 +0200

    Connected to API in specifics.js

--- scripts/specific.js
+++ scripts/specific.js
@@ -1,7 +1,18 @@
-// refer to question 2 before development starts for scope document
+function showSpecific(card) {
+	console.log(card.name);
+}
+
 // get URL query string
 function getQueryStringValue (key) {
     return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
 }
 // variable for the id
 var id = getQueryStringValue("id");
+
+fetch('https://api.magicthegathering.io/v1/cards/' + id)
+	.then(result => result.json())
+	.then((res) => {
+		showSpecific(res.card);
+	})
+	.catch(err => console.log(err));
+
