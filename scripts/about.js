2ba75da697d056ef32c6163e688634bc56c7df17
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-05-24 10:47:38 +0200

    Completed about.js script

--- scripts/about.js
+++ scripts/about.js
@@ -1,1 +1,21 @@
 // refer to question 3 before development starts for scope document
+(function() {
+	const aboutText = document.querySelector('#aboutText');
+	const moreInfoTrigger = document.querySelector('#moreInfoTrigger');
+	const moreInfoContent = document.querySelector('#moreInfoContent');
+	const replacedWord = /Magic/g;
+	let newWord = "Something";
+
+	// Replace 'Magic' with 'Something'
+	aboutText.innerHTML = aboutText.innerHTML.replace(replacedWord, newWord);
+
+	// Show and hide content when user clicks the heading
+	moreInfoTrigger.addEventListener('click', function(e) {
+		if (moreInfoContent.style.display === 'block') {
+			moreInfoContent.style.display = 'none';
+		} else {
+			moreInfoContent.style.display = 'block';
+		}
+	});
+})();
+
