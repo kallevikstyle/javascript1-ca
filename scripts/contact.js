7eb8a56c54910675d03f84b334914e2fe3df3b83
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-05-26 00:47:26 +0200

    Finished contact.js script

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,21 +1,48 @@
 // refer to question 4 before development starts for scope document
 function formValidation() {
-	const firstName = document.querySelector("#firstName").value,
-		lastName = document.querySelector("#lastName").value,
-		phone = document.querySelector("#phone").value,
-		email = document.querySelector("#email").value,
+	const valFirstName = document.querySelector("#firstName").value,
+		valLastName = document.querySelector("#lastName").value,
+		valPhone = document.querySelector("#phone").value,
+		valEmail = document.querySelector("#email").value,
 		namePattern = /^[a-z -]+$/i,
 		emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-		phonePattern = /[0-9]{3}[\\s\\-\\.][0-9]{3}[\\s\\-\\.][0-9]{3}/;
+		phonePattern = /[0-9]{3}[\s\-\.][0-9]{3}[\s\-\.][0-9]{4}/;
+	// Create an object containing boolean values for valid or invalid input	
+	let validForm = {
+		firstName: namePattern.test(valFirstName),
+		lastName : namePattern.test(valLastName),
+		phone: phonePattern.test(valPhone),
+		email: emailPattern.test(valEmail)
+	},
+	errors = [],
+	errorId = "";
 
-		//TO DO
-		// Test patterns
-		// Return array of errors
-	
-
+	// Check for any errors
+	for (key in validForm) {
+		errorId = key + "Error";
+		if (!validForm[key]) {
+			// Display the error message
+			document.getElementById(errorId).style.display = "block";
+			// Add error to array
+			errors.push(key);
+		} else {
+			// Hide error message
+			document.getElementById(errorId).style.display = "none";
+		}
+	}
+	// Action for when all form inputs are valid
+	if (errors.length === 0) {
+		console.log("The form is perfectly valid!");
+	}		
 }
 
+
 (function() {
-	formValidation();
+	const submitButton = document.querySelector("#submitContact");
+
+	submitButton.addEventListener('click', function() {
+		formValidation();	
+	});
+	
 	
 })();
